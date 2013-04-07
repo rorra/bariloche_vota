356598c1336d3b3114954c3017f7dd4544cd810c
Rodrigo Dominguez <rorra@rorra.com.ar>
2013-04-07 05:06:06 -0700

    Admin translations

--- app/admin/schools.rb
+++ app/admin/schools.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register School do
-  
+  index do
+    selectable_column
+    column :id
+    column :name
+    column "Votos Sí", :votes_yes do |school|
+      school.tables.sum(:votes_yes)
+    end
+    column "Votos No", :votes_yes do |school|
+      school.tables.sum(:votes_no)
+    end
+    column "Votos en Blanco", :votes_yes do |school|
+      school.tables.sum(:votes_blank)
+    end
+    column "Votos Nulos", :votes_yes do |school|
+      school.tables.sum(:votes_nil)
+    end
+    column :updated_at
+    default_actions
+  end
 end
--- app/admin/tables.rb
+++ app/admin/tables.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Table do
-  
+  index do
+    selectable_column
+    column :id
+    column :school
+    column :name
+    column :votes_yes
+    column :votes_no
+    column :votes_blank
+    column :votes_nil
+    column :updated_at
+    default_actions
+  end
 end
