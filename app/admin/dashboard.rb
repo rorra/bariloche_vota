e66209b8fa6e4a98fea9608fa26795a770c6af48
Rodrigo Dominguez <rorra@rorra.com.ar>
2013-04-07 06:36:46 -0700

    Backend updated

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,42 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+  menu :priority => 1, :label => proc { I18n.t("active_admin.dashboard") }
 
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content :title => "Votos totales" do
+    columns do
+      column do
+        panel "Info" do
+          table do
+            tr do
+              td "Votos Sí"
+              td "Votos No"
+              td "Votos Blancos"
+              td "Votos Nulos"
+            end
+            tr do
+              td Table.sum(:votes_yes)
+              td Table.sum(:votes_no)
+              td Table.sum(:votes_blank)
+              td Table.sum(:votes_nil)
+            end
+          end
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    columns do
+      column do
+        panel "Info" do
+          table_for Table.joins(:school).order("schools.name, tables.name") do |t|
+            t.column("Escuela") { |row| row.school.name }
+            t.column("Mesa") { |row| row.name }
+            t.column("Votos Sí") { |row| row.votes_yes }
+            t.column("Votos No") { |row| row.votes_no }
+            t.column("Votos en Blanco") { |row| row.votes_blank }
+            t.column("Votos Nulos") { |row| row.votes_nil }
+          end
+        end
+      end
+    end
+  end
 end
