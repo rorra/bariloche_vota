15c874b2748f7ecfbf637cd3dced40d92e1afb7c
Rodrigo Dominguez <rorra@rorra.com.ar>
2013-04-07 02:55:38 -0700

    Fixing active admin bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 BarilocheVota::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
 
   root to: "home#index"
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 end
